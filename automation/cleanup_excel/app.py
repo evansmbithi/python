3f61e4661efff127468850f05b8db57e3ec4d9c4
evansmbithi <evholmbithy@gmail.com>
2023-08-22 10:59:34 +0300

    Fixed .str accessor error
    Error: Can only use .str accessor with string values!

--- automation/cleanup_excel/app.py
+++ automation/cleanup_excel/app.py
@@ -30,7 +30,7 @@ def upload_file():
         # Extract labels (header row)
         labels = df.columns.tolist()
         # print(labels)
-
+        
         return render_template('columns.html', elements=labels, file_name=file_name)
     
     elif 'submit' in request.form: #'submit' button name
@@ -56,27 +56,24 @@ def upload_file():
                 elements=labels,
                 file_name=file_name                
                 )
-        def replace_values(value):
-            if isinstance(value, str):
-                return value.replace(',', '.')
-            return value
+
         #clean up
         def clean_data(option,rep,*chars,col=df):
             for char in chars:
-                # col['STEP_ID'] = col['STEP_ID'].str.replace(char,'_') # Can only use .str accessor with string values!
-                # col[option] = col[option].apply(str).str.replace(char,rep) # had NaN issue
-                col[option] = col[option].apply(
-                                            lambda x: x.replace(char,rep)
-                                            if isinstance(x, str)
-                                            else x
-                                        )
+                # col['STEP_ID'] = col['STEP_ID'].str.replace(char,'_') # Error: Can only use .str accessor with string values!
+                col[option] = col[option].apply(str).str.replace(char,rep) # Convert all values to string # has a NaN issue                
+                # col[option] = col[option].apply(
+                #                             lambda x: x.replace(char,rep)
+                #                             if isinstance(x, str)
+                #                             else x
+                #                         )
                 # It only calls the replace() method if the supplied value is a string.
-                # Otherwise, the value is returned as is.
-                # https://bobbyhadz.com/blog/can-only-use-str-accessor-with-string-values-in-python
+                # Otherwise, the value is returned as is.                
             return col
         
         # Remove special characters from selected Columns
-        for column in selected_options:
+        for column in selected_options:            
+            alert_msg = "Blank cells will be shown as 'nan'" 
             try:
                 [clean_data(column,replacer,replacee,'__') for _ in range(4)] # run four times
             except KeyError:
@@ -95,9 +92,11 @@ def upload_file():
         # Return a link to download the processed data
         # btn = f'<a href="/download">Download processed data</a>'
         success_message = "Clean up completed successfully! ✨"
+        
         return render_template(
                                 'index.html',
-                                success_message = success_message
+                                success_message = success_message,
+                                alert_msg=alert_msg
                                 )
     elif 'back' in request.form:
         return render_template('index.html')
@@ -110,3 +109,9 @@ def download_file():
 if __name__ == '__main__':
     app.run(debug=True)
 
+##### REF #######
+# Error: Can only use .str accessor with string values!
+# https://towardsdatascience.com/5-methods-to-check-for-nan-values-in-in-python-3f21ddd17eed
+
+
+# https://bobbyhadz.com/blog/can-only-use-str-accessor-with-string-values-in-python
