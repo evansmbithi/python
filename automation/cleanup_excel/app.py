473258476e1ccabf1756261ae8a9773ec020698a
evansmbithi <evholmbithy@gmail.com>
2023-09-15 17:33:00 +0300

    added raw input provision

--- automation/cleanup_excel/app.py
+++ automation/cleanup_excel/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, send_file
 import pandas as pd
+import re
 
 app = Flask(__name__)
 # regex = '[^a-zA-Z0-9]'
@@ -14,24 +15,39 @@ def upload_file():
     global df
     global labels
     global file_name
+    global textarea
 
     if 'upload' in request.form: #'upload' button name
         file = request.files['file']
         file_name = file.filename
-        try:
-            df = pd.read_excel(file)
-        except:
-            file_error = 'Please upload an excel file'
+        textarea = request.form['textarea']            
+
+        if textarea:
+            # print (textarea)
+            # execute cleanup
+            return render_template('replace.html', output=textarea)
+        elif file:
+            try:
+                df = pd.read_excel(file)
+            except:
+                file_error = 'Please upload an excel file'
+                return render_template(
+                        'index.html',
+                        display_error = file_error
+                        )
+            
+            # Extract labels (header row)
+            labels = df.columns.tolist()
+            # print(labels)
+            
+            return render_template('columns.html', elements=labels, file_name=file_name)
+        else:
+            file_error = 'Please type something or upload an excel file'
             return render_template(
                     'index.html',
                     display_error = file_error
                     )
-        
-        # Extract labels (header row)
-        labels = df.columns.tolist()
-        # print(labels)
-        
-        return render_template('columns.html', elements=labels, file_name=file_name)
+
     
     elif 'submit' in request.form: #'submit' button name
         selected_options = request.form.getlist('checkboxes')
@@ -46,55 +62,63 @@ def upload_file():
                 elements=labels,
                 file_name=file_name
                 )
-            
-        if selected_options == []:
-            error_msg = "Please select a column to proceed"
-            return render_template(
-                'columns.html',
-                elements=labels,
-                column_error=error_msg,
-                file_name=file_name                
-                )
-
-        #clean up
-        def clean_data(option,rep,*chars,col=df):
-            for char in chars:
-                # col['STEP_ID'] = col['STEP_ID'].str.replace(char,'_') # Error: Can only use .str accessor with string values!    
-                try:
-                    col[option] = col[option].str.replace(char,rep)   
-                except:
-                    # print('Blank cell') 
-                    continue        
-            return col
         
-        # Remove special characters from selected Columns
-        for column in selected_options:            
-            alert_msg = "Blank cells will be shown as 'nan'" 
-            try:
-                [clean_data(column,replacer,replacee,'__') for _ in range(4)] # run four times
-            except KeyError:
-                column_error = f"'{column}' is not available as a column in this file."
+        # choose between textarea and excel
+        if bool(textarea) == True:
+            # first argument is the regex pattern, the second is the new string, and the third is the string to be processed.
+            cleenit = re.sub(replacee,replacer,textarea)
+
+            return render_template('replace.html', output=cleenit)
+        else:    
+            if selected_options == []:
+                error_msg = "Please select a column to proceed"
                 return render_template(
-                    'index.html',
-                    display_error = column_error
+                    'columns.html',
+                    elements=labels,
+                    column_error=error_msg,
+                    file_name=file_name                
                     )
-            
 
-        # Save the cleaned DataFrame to a new Excel file
-        df.to_excel("./output/cleaned_example.xlsx", index=False)
-        # df.to_excel(f"./output/{file.filename}", index=False)
-
-        print('\nClean up completed successfully! ✨')
-        # Return a link to download the processed data
-        # btn = f'<a href="/download">Download processed data</a>'
-        success_message = "Clean up completed successfully! ✨"
+            #clean up
+            def clean_data(option,rep,*chars,col=df):
+                for char in chars:
+                    # col['STEP_ID'] = col['STEP_ID'].str.replace(char,'_') # Error: Can only use .str accessor with string values!    
+                    try:
+                        col[option] = col[option].str.replace(char,rep)   
+                    except:
+                        # print('Blank cell') 
+                        continue        
+                return col
         
-        return render_template(
-                                'index.html',
-                                success_message = success_message,
-                                )
+            # Remove special characters from selected Columns
+            for column in selected_options:            
+                alert_msg = "Blank cells will be shown as 'nan'" 
+                try:
+                    [clean_data(column,replacer,replacee,'__') for _ in range(4)] # run four times
+                except KeyError:
+                    column_error = f"'{column}' is not available as a column in this file."
+                    return render_template(
+                        'index.html',
+                        display_error = column_error
+                        )
+                
+
+            # Save the cleaned DataFrame to a new Excel file
+            df.to_excel("./output/cleaned_example.xlsx", index=False)
+            # df.to_excel(f"./output/{file.filename}", index=False)
+
+            print('\nClean up completed successfully! ✨')
+            # Return a link to download the processed data
+            # btn = f'<a href="/download">Download processed data</a>'
+            success_message = "Clean up completed successfully! ✨"
+            
+            return render_template(
+                                    'index.html',
+                                    success_message = success_message,
+                                    )
     elif 'back' in request.form:
         return render_template('index.html')
+    
 
 @app.route('/download')
 def download_file():
@@ -105,6 +129,7 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 ##### REF #######
+# re.sub() - https://note.nkmk.me/en/python-str-replace-translate-re-sub/#replace-strings-by-regex-resub-resubn
 # Error: Can only use .str accessor with string values!
 # https://towardsdatascience.com/5-methods-to-check-for-nan-values-in-in-python-3f21ddd17eed
 
